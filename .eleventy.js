330b500910fb911edd9e5a30c43e6ddfb5520535
Daniel Way <projects@danieldway.com>
2023-05-25 21:04:50 -0500

    Install and configure 11ty navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,19 @@
 const sass = require("sass");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("assets");
-  
+
   eleventyConfig.addTemplateFormats("scss");
   eleventyConfig.addExtension("scss", {
     outputFileExtension: "css",
-    compile: async function(inputContent) {
+    compile: async function (inputContent) {
       let result = sass.compileString(inputContent);
       return async (_) => result.css;
-    }
+    },
   });
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
 };
